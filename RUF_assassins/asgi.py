8504d5d2f53d99115c621d0e121f5160c1c45068
AJ Steinhuaser <ajsteinhauser11@gmail.com>
2022-01-16 21:58:00 -0500

    Inital commit

---
+++ RUF_assassins/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for RUF_assassins project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'RUF_assassins.settings')
+
+application = get_asgi_application()
